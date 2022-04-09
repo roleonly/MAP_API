efd6dbe46810048dd3ecbb0832c34d62dfc78a81
Ahmet İhsan GÜNEY <ahmetihsanguney@gmail.com>
2022-04-09 11:41:24 +0300

    rasters append to db.worldmap

--- Customer/tests.py
+++ Customer/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from distutils import filelist
 
-# Create your tests here.
+import subprocess
+import os
+
+
+
+
+    
+
+
+
+cmds='raster2pgsql.exe -I -a -C -s 4326 -t 100x100 -F -M E:\\DATA\\turkey\\7\\* public.worldmap | psql' + ' "host=127.0.0.1 port=5432 dbname=DB_API user=gisadmin password=Role1453"'
+
+subprocess.call(cmds, shell=True)
+print (cmds)
