b732ba9361591a90417d07c702b37364cc4bfc1b
Ahmet İhsan GÜNEY <ahmetihsanguney@gmail.com>
2022-04-09 11:46:26 +0300

    some tests done

--- Customer/tests.py
+++ Customer/tests.py
@@ -1,16 +1,3 @@
-from distutils import filelist
+from django.test import TestCase
 
-import subprocess
-import os
-
-
-
-
-    
-
-
-
-cmds='raster2pgsql.exe -I -a -C -s 4326 -t 100x100 -F -M E:\\DATA\\turkey\\7\\* public.worldmap | psql' + ' "host=127.0.0.1 port=5432 dbname=DB_API user=gisadmin password=Role1453"'
-
-subprocess.call(cmds, shell=True)
-print (cmds)
+# Create your tests here.
